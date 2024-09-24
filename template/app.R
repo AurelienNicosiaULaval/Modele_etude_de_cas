e95ea0326b68285a5f50b68ad7c6009aec10c5a4
AurelienNicosiaULaval <aurelien.nicosia@mat.ulaval.ca>
2024-09-24 08:11:52 -0400

    add shiny version for deployment

---
+++ template/app.R
@@ -0,0 +1,15 @@
+library(shiny)
+library(learnr)
+
+# Charger le fichier RMarkdown en tant que tutoriel learnr
+tutorial_file <- "template.Rmd"
+
+# Définir l'application Shiny pour afficher le tutoriel
+shinyApp(
+  ui = fluidPage(
+    learnr::run_tutorial(tutorial_file)
+  ),
+  server = function(input, output, session) {
+    # Rien de spécifique dans server car tout est géré par le tutoriel learnr
+  }
+)
